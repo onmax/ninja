cbb4ff87f4ad1fc30b2d1c02aae2a9ba412a8068
onmax <maximogarciamtnez@gmail.com>
2024-11-02 14:44:55 +0100

    chore: added vectorize

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,6 +29,15 @@ export default defineNuxtConfig({
   // https://hub.nuxt.com/docs/getting-started/installation#options
   hub: {
     ai: true,
+    vectorize: {
+      posts: {
+        dimensions: 768, // @cf/baai/bge-base-en-v1.5
+        metric: 'cosine',
+        metadataIndexes: {
+          id: 'string',
+        },
+      },
+    },
   },
 
   nitro: {
